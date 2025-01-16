751a9356d40b21625b4728a2344b5948e6a456b2
Harsh Kumar Singh <118126519+harshkumarsingh001@users.noreply.github.com>
2025-01-16 22:43:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (76.7%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -4,7 +4,8 @@ class Solution {
         for (int j = 0; j < nums.length; j++) {
     int n = nums[j];
     if (i < 2 || n != nums[i - 2]) {
-        nums[i++] = n;
+       nums[i] = n;
+          i++;
     }
 }
         return i;
